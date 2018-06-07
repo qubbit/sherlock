0d9e66ceb9012054553322630b91893d859735d9
Gopal Adhikari <gadhikari@covermymeds.com>
2018-06-07 17:20:58 -0400

    Use SCSS to generate CSS

--- setup.sh
+++ setup.sh
@@ -1,7 +1,3 @@
 #!/usr/bin/env bash
 
-npm install -g postcss
-npm install -g precss
-npm install -g autoprefixer
-npm install -g postcss-cli
-npm install -g postcss-color-function
+sass --watch assets/css/screen.scss:assets/css/screen.css
